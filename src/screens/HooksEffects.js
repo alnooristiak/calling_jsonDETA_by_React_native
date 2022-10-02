5fee59634e672c783e454b6ad7b1c5da465baf99
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-10-02 20:07:31 +0600

    adding comment to help me feature

--- src/screens/HooksEffects.js
+++ src/screens/HooksEffects.js
@@ -2,60 +2,63 @@ import { StyleSheet, Text, View, FlatList } from 'react-native'
 import React, { useEffect, useState } from 'react'
 
 export default function HooksEffects() {
+  // To call data and get data 
+  const [myuserDeta, setMyUserDeta] = useState();
+  // loder show when data Dasn't show in ui 
+  const [isLoding, setIsLoding] = useState(true);
 
-    const [myuserDeta, setMyUserDeta] = useState();
-    const [isLoding, setIsLoding] = useState(true);
+  // calling data with async await function 
+  const callDeta = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const myDeta = await response.json();
+      setMyUserDeta(myDeta);
+      setIsLoding(false);
+      //   console.log(myDeta);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-    const callDeta = async () => {
-        try {
-          const response = await fetch ('https://jsonplaceholder.typicode.com/users');
-          const myDeta = await response.json();
-          setMyUserDeta(myDeta);
-          setIsLoding(false);
-        //   console.log(myDeta);
-        } catch (error) {
-          console.log(error);
-        }
-      }
+  // const callDeta = () => {
+  //     fetch ('https://jsonplaceholder.typicode.com/users')
+  //         .then(res => res.json())
+  //         .then(deta => console.log(deta))
+  // }
 
-    // const callDeta = () => {
-    //     fetch ('https://jsonplaceholder.typicode.com/users')
-    //         .then(res => res.json())
-    //         .then(deta => console.log(deta))
-    // }
+  // useEffect hook to using calling data function to show ui 
+  useEffect(() => {
+    callDeta();
+  }, []);
 
-      useEffect(() => {
-        callDeta();
-      }, []);
-    
 
   return (
     <View>
-      <Text style={{color: 'brown', textAlign: 'center', fontSize: 20, fontWeight: 600,}}>10 User Data Show</Text>
-      <FlatList 
-      data={myuserDeta}
-      renderItem={( {item} ) => {
-        return(
-          <View>
-            <View style={styles.card}>
-              <View style={styles.tittles}>
-                <Text>
-                  <Text style={styles.bolderLinText}>Name: </Text>{item.name}
-                </Text>
-                <Text>
-                  <Text style={styles.bolderLinText}>Email: </Text>{item.email}
-                </Text>
-              </View>
-              <View>
-                <Text style={styles.addressLn}>
-                  <Text style={styles.bolderLinText}>Address: </Text>{item.address.city}
-                </Text>
-                {/* <Text>{item.email}</Text> */}
+      <Text style={{ color: 'brown', textAlign: 'center', fontSize: 20, fontWeight: 600, }}>10 User Data Show</Text>
+      <FlatList
+        data={myuserDeta}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <View style={styles.card}>
+                <View style={styles.tittles}>
+                  <Text>
+                    <Text style={styles.bolderLinText}>Name: </Text>{item.name}
+                  </Text>
+                  <Text>
+                    <Text style={styles.bolderLinText}>Email: </Text>{item.email}
+                  </Text>
+                </View>
+                <View>
+                  <Text style={styles.addressLn}>
+                    <Text style={styles.bolderLinText}>Address: </Text>{item.address.city}
+                  </Text>
+                  {/* <Text>{item.email}</Text> */}
+                </View>
               </View>
             </View>
-          </View>
-        )
-      }}
+          )
+        }}
       />
     </View>
   )
