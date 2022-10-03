63da3defb041f4fd57c78018f768bc637b6fb25f
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-10-03 20:59:23 +0600

    submit buttion customization and valide user alert section

--- src/screens/LoginFrom/LoginFrom.js
+++ src/screens/LoginFrom/LoginFrom.js
@@ -3,48 +3,70 @@ import Checkbox from 'expo-checkbox';
 import React, { useState } from 'react'
 
 export default function LoginFrom() {
+    const [userName, setUserName] = useState('');
+    const [userPassword, setUserPassword] = useState('');
+    // console.log(userName, userPassword);
+
+    // for using submit button to verifide user 
+    const submit = () => {
+        if (userName === "istiak" && userPassword === "is123"){
+            alert(`Welcome Back ${userName}`)
+        }else{
+            alert(`user name and password is'n currect please try again`)
+        }
+    }
+
     // checkBox confarmation 
     const [agree, setAgree] = useState(false);
+
     return (
         <View style={{ marginTop: 40, }}>
             <Text style={styles.mainFromText}>Login Forn</Text>
             <Text style={styles.smallTxt}>Please filup currect email and password..</Text>
             <View>
                 <View>
-                    <Text style={styles.inputTopLable}>Enter Your Vailide Email</Text>
+                    <Text style={styles.inputTopLable}>Enter Your Name</Text>
                     <TextInput
                         style={styles.inputStyle}
                         autoCapitalize='none'
                         autoCorrect={false}
+                        value={userName}
+                        onChangeText={(actualDeta) => setUserName(actualDeta)}
                     />
                 </View>
+
                 <View style={styles.inputDiv}>
-                    <Text style={styles.inputTopLable}>Enter Your Vailide Email</Text>
+                    <Text style={styles.inputTopLable}>Enter Valide Password</Text>
                     <TextInput
                         style={styles.inputStyle}
                         secureTextEntry={true}
                         autoCapitalize='none'
                         autoCorrect={false}
+                        valu={userPassword}
+                        onChangeText={(actualDeta) => setUserPassword(actualDeta)}
                     />
                 </View>
             </View>
+
             <View style={styles.dflexCBox}>
                 <Checkbox
-                //   style={styles.checkbox}
-                  value={agree}
-                  onValueChange={() => setAgree(!agree)}
-                  color={agree ? '#16a085' : undefined} 
+                    //   style={styles.checkbox}
+                    value={agree}
+                    onValueChange={() => setAgree(!agree)}
+                    color={agree ? '#16a085' : undefined}
                 />
-                <Text 
-                style={[styles.cBoxTxt, {color: agree ? '#16a085' : '#7f8c8d'}]}>Ihave Agree all trams and condation!</Text>
+                <Text
+                    style={[styles.cBoxTxt, { color: agree ? '#16a085' : '#7f8c8d' }]}>Ihave Agree all trams and condation!</Text>
             </View>
+            
             <View>
                 <TouchableOpacity
                     style={[styles.loginBtn,
                     {
                         backgroundColor: agree ? '#16a085' : '#7f8c8d'
-                }]}
-                disabled={!agree}
+                    }]}
+                    disabled={!agree}
+                    onPress={() => submit()}
                 >
                     <Text>Login</Text>
                 </TouchableOpacity>
