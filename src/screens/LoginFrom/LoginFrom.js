d79d04bb4ce592d3cbe5ea54eec251220f675f41
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-10-03 19:56:49 +0600

    adding check box and button condition are cheked color changed

--- src/screens/LoginFrom/LoginFrom.js
+++ src/screens/LoginFrom/LoginFrom.js
@@ -1,56 +1,79 @@
-import { StyleSheet, Text, TextInput, View, secureTextEntry } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, View, secureTextEntry, TouchableOpacity } from 'react-native'
+import Checkbox from 'expo-checkbox';
+import React, { useState } from 'react'
 
 export default function LoginFrom() {
-  return (
-      <View>
-          <Text style={styles.mainFromText}>Login Forn</Text>
-          <Text style={styles.smallTxt}>Please filup currect email and password..</Text>
-          <View>
-              <View>
-                  <Text style={styles.inputTopLable}>Enter Your Vailide Email</Text>
-                  <TextInput
-                      style={styles.inputStyle}
-                      secureTextEntry={true}
-                      autoCapitalize='none'
-                      autoCorrect={false}
-                  />
-              </View>
-              <View>
-                  <Text style={styles.inputTopLable}>Enter Your Vailide Email</Text>
-                  <TextInput
-                      style={styles.inputStyle}
-                      secureTextEntry={true}
-                      autoCapitalize='none'
-                      autoCorrect={false}
-                  />
-              </View>
-          </View>
-      </View>
-  )
+    // checkBox confarmation 
+    const [agree, setAgree] = useState(false);
+    return (
+        <View style={{ marginTop: 40, }}>
+            <Text style={styles.mainFromText}>Login Forn</Text>
+            <Text style={styles.smallTxt}>Please filup currect email and password..</Text>
+            <View>
+                <View>
+                    <Text style={styles.inputTopLable}>Enter Your Vailide Email</Text>
+                    <TextInput
+                        style={styles.inputStyle}
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                    />
+                </View>
+                <View style={styles.inputDiv}>
+                    <Text style={styles.inputTopLable}>Enter Your Vailide Email</Text>
+                    <TextInput
+                        style={styles.inputStyle}
+                        secureTextEntry={true}
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                    />
+                </View>
+            </View>
+            <View style={styles.dflexCBox}>
+                <Checkbox
+                //   style={styles.checkbox}
+                  value={agree}
+                  onValueChange={() => setAgree(!agree)}
+                  color={agree ? '#16a085' : undefined} 
+                />
+                <Text 
+                style={[styles.cBoxTxt, {color: agree ? '#16a085' : '#7f8c8d'}]}>Ihave Agree all trams and condation!</Text>
+            </View>
+            <View>
+                <TouchableOpacity
+                    style={[styles.loginBtn,
+                    {
+                        backgroundColor: agree ? '#16a085' : '#7f8c8d'
+                }]}
+                disabled={!agree}
+                >
+                    <Text>Login</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-    mainFromText:{
+    mainFromText: {
         color: '#16a085',
         fontSize: 35,
         fontWeight: 'bold',
         marginBottom: 15,
     },
-    smallTxt:{
+    smallTxt: {
         color: '#7f8c8d',
         fontSize: 17,
         marginBottom: 20,
     },
-    inputTopLable:{
+    inputTopLable: {
         color: '#16a085',
         marginBottom: 7,
         fontSize: 16,
     },
-    tk:{
-        color: 'blue'
+    inputDiv: {
+        marginTop: 19,
     },
-    inputStyle:{
+    inputStyle: {
         // borderColor: 'rgba(0,0,0,0.3)',
         borderColor: '#1abc9c',
         borderWidth: 2,
@@ -58,5 +81,28 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         fontSize: 18,
         color: '#079992'
-    }
+    },
+    dflexCBox: {
+        // flex: 1,
+        // display: 'flex',
+        flexDirection: 'row',
+        marginTop:7
+    },
+    cBoxTxt: {
+        marginStart: 7
+    },
+    loginBtn:{
+        color: 'white',
+        marginTop: 15,
+        paddingVertical: 10,
+        textAlign: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+        borderRadius: 6
+    },
+    tk: {
+        color: 'blue'
+    },
+    
 })
