69a1311721c01f5dd1c01faba6a38095d5568173
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-10-03 23:47:53 +0600

    finish and final touch

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # calling_jsonDETA_by_React_native
+## by Al Noor Istiak Mahmud.
+
+#### Thank You Visite and see my git reposetorry.
+
--- src/screens/HomePage/Home.js
+++ src/screens/HomePage/Home.js
@@ -1,11 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 export default function Home() {
   return (
     <View>
-      <Text>Home</Text>
+      {/* <Text>Home</Text> */}
       <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, magnam repudiandae? Dolor at, iure culpa illum eius tenetur? Nihil minus necessitatibus ratione pariatur repellat eaque architecto nisi delectus unde sunt.</Text>
+          <TouchableOpacity
+            title="Go to Details"
+          />
     </View>
   )
 }
